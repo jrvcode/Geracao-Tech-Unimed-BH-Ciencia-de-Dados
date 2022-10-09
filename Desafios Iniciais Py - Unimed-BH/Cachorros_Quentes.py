579c9bc08b2e22e6aa370574021edd6de13c6792
jrvcode <contato.juniorvieira@gmail.com>
2022-10-09 13:09:51 -0400

    Atualização

---
+++ Desafios Iniciais Py - Unimed-BH/Cachorros_Quentes.py
@@ -0,0 +1,12 @@
+# Cachorros Quentes
+# Desafio 2/3:
+# https://github.com/jrvcode/Geracao-Tech-Unimed-BH-Ciencia-de-Dados/blob/main/Desafios%20Iniciais%20Py%20-%20Unimed-BH/README.md
+
+valores = input().split() 
+
+c = int(valores[0])
+p = int(valores[1])
+
+media = c/p
+
+print('%0.2f' % media)
